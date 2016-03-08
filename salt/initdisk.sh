0dd4400dc6063686a7f95b012ec7c158bd81cc19
Joe Olson <joe.olson@physiq.com>
2016-03-08 12:48:56 -0600

    changed salt

--- salt/initdisk.sh
+++ salt/initdisk.sh
@@ -1,1 +1,12 @@
-echo "test" >/home/ubuntu/test.txt
+#!/bin/sh
+hdd="/dev/xvdb"
+for i in $hdd;do
+echo "n
+p
+1
+
+
+w
+"|sudo fdisk $i;sudo mkfs.ext4 $i;done
+sudo mkdir /mnt/data
+sudo mount /dev/xvdb /mnt/data
