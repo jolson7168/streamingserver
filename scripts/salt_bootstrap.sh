8f66580b0d2af12074ef476d3e56d612a2f6df70
Joe Olson <joe.olson@physiq.com>
2016-03-16 14:45:21 -0500

    temp to debug issue

--- scripts/salt_bootstrap.sh
+++ scripts/salt_bootstrap.sh
@@ -6,8 +6,9 @@ wget -O install_salt.sh https://bootstrap.saltstack.com
 sudo sh install_salt.sh
 sudo echo "file_client: local" >>/etc/salt/minion
 sudo echo "local: True" >>/etc/salt/minion
+sudo service salt-minion restart
 sudo apt-get install -y git
 sudo mkdir /srv/salt
 git clone https://github.com/jolson7168/streamingserver.git /home/ubuntu/streamingserver
 sudo cp -R /home/ubuntu/streamingserver/salt/* /srv/salt
-#sudo salt-call --local state.highstate
+sudo salt-call --local state.highstate
